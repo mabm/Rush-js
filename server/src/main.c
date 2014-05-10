d1400106f05195f8ed3820225a642fdb603891d6
Nicolas ades <nicolas.ades@epitech.eu>
2014-05-10 13:26:20 +0200

    nico

---
+++ server/src/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for  in /home/nicolas/Workspace/rush/rush_promo_js/server/src
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Sat May 10 13:24:57 2014 Nicolas Ades
+** Last update Sat May 10 13:25:53 2014 Nicolas Ades
+*/
+
+int	main()
+{
+  return (0);
+}
