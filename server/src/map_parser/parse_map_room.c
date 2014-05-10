fa4d544b4ab4afbee524f29ec9971eb244808d95
Laurent Fourrier <fourri_l@epitech.eu>
2014-05-10 19:39:30 +0200

    Added server parsing for champion and room

--- server/src/map_parser/parse_map_room.c
+++ server/src/map_parser/parse_map_room.c
@@ -5,82 +5,33 @@
 ** Login   <fourri_l@epitech.net>
 ** 
 ** Started on  Sat May 10 16:22:45 2014 Laurent Fourrier
-** Last update Sat May 10 18:29:23 2014 Laurent Fourrier
+** Last update Sat May 10 19:21:26 2014 Laurent Fourrier
 */
 
-#include <unistd.h>
 #include <stdlib.h>
 #include "world.h"
+#include "utils.h"
 
-/*
-** Structure for calling the right function
-*/
-struct	s_parsemapfunc
-{
-  char	byte;
-  int	(*func)(t_room *room, int fd);
-}	g_parsemapfunc[] =
-  {
-    {0x01, &parse_map_room_name},
-    {0x10, &parse_map_room_adv},
-    {0x11, &parse_map_room_connection},
-    {0x12, &parse_map_room_monster},
-    {0x00, NULL}
-  };
-
-static int	parse_map_room_name(t_room *room, int fd)
-{
-  room->name = read_string(fd);
-  if (!room->name)
-    return (0);
-  return (1);
-}
-
-static int	parse_map_room_adv(t_room *room, int fd)
-{
-  room->adv = read_string(fd);
-  if (!room->adv)
-    return (0);
-  return (1);
-}
-
-static int	parse_map_room_connection(t_room *room, int fd)
+int		parse_map_room(t_world *world, char *line)
 {
   char		*buf;
+  int		it;
+  t_room	*rtn;
 
-  buf = read_string(fd);
-  if (!buf)
-    return (0);
-  room->links = str_to_wordarr(buf, "=");
-  return (1);
-}
-
-static int	parse_map_room_monster(t_room *room, int fd)
-{
-  char		*buf;
-
-  buf = read_string(fd);
-  if (!buf)
-    return (0);
-  room->monster = str_to_wordarr(buf, "=");
-  return (1);
-}
-
-int		parse_map_room(t_world *world, int fd)
-{
-  char		byte;
-  t_room	*room;
-
-  room = malloc(sizeof(room));
-  if (!room)
+  rtn = malloc(sizeof(t_room));
+  if (!rtn)
     exit(0);
-  while (read(fd, &byte, 1) < 1 && byte != '\n')
-    {
-      while (g_parsemapfunc[it].byte == byte)
-	{
-	  if /* TODO */
-	  it++;
-	}
-    }
-  return (0);
+  it = 2;
+  rtn->name = read_string(line + it);
+  it += line[it] + 2;
+  rtn->adv = read_string(line + it);
+  it += line[it] + 2;
+  buf = read_string(line + it);
+  rtn->links = str_to_wordarr(buf, "=");
+  free(buf);
+  it += line[it] + 2;
+  buf = read_string(line + it);
+  rtn->monster = str_to_wordarr(buf, "=");
+  free(buf);
+  return (rtn);
 }
