9d180ff94839f83da2143ab7fb4e4382408f1a69
Laurent Fourrier <fourri_l@epitech.eu>
2014-05-10 15:49:17 +0200

    Fixed some things in map_parser

---
+++ server/src/map_parser/parse_map_elem.c
@@ -0,0 +1,16 @@
+/*
+** parse_map_elem.c for rush-js in /home/fourri_l/rendu/Rush-js/server
+** 
+** Made by Laurent Fourrier
+** Login   <fourri_l@epitech.net>
+** 
+** Started on  Sat May 10 15:45:08 2014 Laurent Fourrier
+** Last update Sat May 10 15:48:22 2014 Laurent Fourrier
+*/
+
+#include "world.h"
+
+void	parse_map_elem(t_world *world, int fd)
+{
+  /* TODO */
+}
