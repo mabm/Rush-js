4e84737ce9f4b617cb175af129c0b1cb570e293c
Nicolas ades <nicolas.ades@epitech.eu>
2014-05-10 14:08:11 +0200

    modif main

--- server/src/main.c
+++ server/src/main.c
@@ -5,10 +5,17 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Sat May 10 13:24:57 2014 Nicolas Ades
-** Last update Sat May 10 13:25:53 2014 Nicolas Ades
+** Last update Sat May 10 14:07:40 2014 Nicolas Ades
 */
 
-int	main()
+int	main(int ac, char **av)
 {
+  if (ac != 2)
+    {
+      printf("USAGE: ./server [GAME FILE]\n");
+      return (-1);
+    }
+  else
+    pars_map(av[1]);
   return (0);
 }
