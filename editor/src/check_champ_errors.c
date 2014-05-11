04dcc5b68b073717584518bd689531f699d08240
mediav_j <mediav_j@epitech.eu>
2014-05-11 19:26:08 +0200

    check monstre et champion

--- editor/src/check_champ_errors.c
+++ editor/src/check_champ_errors.c
@@ -5,7 +5,7 @@
 ** Login   <mediav_j@epitech.net>
 ** 
 ** Started on  Sun May 11 17:59:00 2014 Jeremy Mediavilla
-** Last update Sun May 11 19:05:31 2014 Jeremy Mediavilla
+** Last update Sun May 11 19:21:36 2014 Jeremy Mediavilla
 */
 
 #include "parser.h"
@@ -24,6 +24,20 @@ char		*is_valid_stats(t_winChampion *data, int i, int j)
   return (NULL);
 }
 
+int		is_already_champ(char *name)
+{
+  t_champ	*tmp;
+
+  tmp = game->champs;
+  while (tmp != NULL)
+    {
+      if (strcmp(tmp->name, name) == 0)
+	return (1);
+      tmp = tmp->next;
+    }
+  return (0);
+}
+
 char		*check_champ_errors(t_winChampion *data)
 {
   char		*error;
@@ -32,6 +46,8 @@ char		*check_champ_errors(t_winChampion *data)
   text = gtk_entry_get_text(GTK_ENTRY(data->attEntry[0]));
   if (text[0] == '\0')
     return ("Missing Champion name");
+  else if (is_already_champ((char *) text) == 1)
+    return ("Champion already exist");
   text = gtk_combo_box_get_active_text(GTK_COMBO_BOX(data->combo_box));
   if (text == NULL)
     return ("Missing Champion type");
--- editor/src/check_monst_errors.c
+++ editor/src/check_monst_errors.c
@@ -5,7 +5,7 @@
 ** Login   <mediav_j@epitech.net>
 ** 
 ** Started on  Sun May 11 18:51:12 2014 Jeremy Mediavilla
-** Last update Sun May 11 19:06:21 2014 Jeremy Mediavilla
+** Last update Sun May 11 19:25:46 2014 Jeremy Mediavilla
 */
 
 #include "parser.h"
@@ -25,6 +25,20 @@ char		*is_valid_stats2(t_winMonster *data, int i, int j)
   return (NULL);
 }
 
+int		is_already_monst(char *type)
+{
+  t_monster	*tmp;
+
+  tmp = game->monsters;
+  while (tmp != NULL)
+    {
+      if (strcmp(tmp->type, type) == 0)
+	return (1);
+      tmp = tmp->next;
+    }
+  return (0);
+}
+
 char		*check_monst_errors(t_winMonster *data)
 {
   char		*error;
@@ -33,6 +47,8 @@ char		*check_monst_errors(t_winMonster *data)
   text = gtk_entry_get_text(GTK_ENTRY(data->attEntry[0]));
   if (text[0] == '\0')
     return ("Missing Monster name");
+  else if (is_already_monst((char *) text) == 1)
+    return ("This monster type already exist");
   if ((error = is_valid_stats2(data, 1, 4)) != NULL)
     return (error);
   text = gtk_entry_get_text(GTK_ENTRY(data->attEntry[5]));
