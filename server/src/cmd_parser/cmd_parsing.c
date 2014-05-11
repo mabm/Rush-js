c1078ccde562cb642317a61958ce97b9af5c21d5
Nicolas ades <nicolas.ades@epitech.eu>
2014-05-11 19:00:31 +0200

    modif cmd_pars

--- server/src/cmd_parser/cmd_parsing.c
+++ server/src/cmd_parser/cmd_parsing.c
@@ -5,7 +5,7 @@
 ** Login   <valer@epitech.net>
 **
 ** Started on  Fri May  9 22:31:21 2014 Valerian Polizzi
-** Last update Sun May 11 18:53:04 2014 Joris Bertomeu
+** Last update Sun May 11 18:59:53 2014 Nicolas Ades
 */
 
 #include <stdio.h>
@@ -43,37 +43,29 @@ int		check_cmd(char *cmd_tab, char *cmd)
 
 int	       parse_cmd(char *cmd, int id, t_libserver *libserver, t_world *world)
 {
-  printf("Commande reçue : %s\n", cmd);
+  char		*tab[7];
+  t_fonc_tab	fonc_tab[6];
+  int		i;
 
-  /* char		*tab[7]; */
-  /* t_fonc_tab	fonc_tab[6]; */
-  /* int		i; */
-
-  /* i = 0; */
-  /* libserver->fdtmp = id; */
-  /* fonc_tab[0] = &is_next; */
-  /* fonc_tab[1] = &is_list_team; */
-  /* fonc_tab[2] = &is_attack; */
-  /* fonc_tab[3] = &is_attack_spe; */
-  /* fonc_tab[4] = &is_who; */
-  /* fonc_tab[5] = &bye; */
-  /* tab[0] = "next"; */
-  /* tab[0][5] = 0; */
-  /* tab[1] = "list_team"; */
-  /* tab[1][10] = 0; */
-  /* tab[2] = "attack"; */
-  /* tab[2][7] = 0; */
-  /* tab[3] = "attack_spe"; */
-  /* tab[3][11] = 0; */
-  /* tab[4] = "who"; */
-  /* tab[4][4] = 0; */
-  /* tab[5] = "bye"; */
-  /* tab[5][4] = 0; */
-  /* tab[6] = NULL; */
-  /* while (i < 7) */
-  /*   { */
-  /*     fonc_tab[i](get_opt(cmd, libserver), libserver); */
-  /*     i++; */
-  /*   } */
-  /* return (0); */
+  i = 0;
+  libserver->fdtmp = id;
+  fonc_tab[0] = &is_next;
+  fonc_tab[1] = &is_list_team;
+  fonc_tab[2] = &is_attack;
+  fonc_tab[3] = &is_attack_spe;
+  fonc_tab[4] = &is_who;
+  fonc_tab[5] = &bye;
+  tab[0] = strdup("next");
+  tab[1] = strdup("list_team");
+  tab[2] = strdup("attack");
+  tab[3] = strdup("attack_spe");
+  tab[4] = strdup("who"); 
+  tab[5] = strdup("bye");
+  tab[6] = NULL;
+  while (i < 7)
+    {
+      fonc_tab[i](get_opt(cmd, libserver), libserver);
+      i++;
+    }
+  return (0);
 }
