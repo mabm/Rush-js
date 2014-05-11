bf9cf5f4b74a4b63c1991d31dd4ecd4439ebf3eb
mediav_j <mediav_j@epitech.eu>
2014-05-11 19:06:53 +0200

    ca compile ma gueule

--- editor/src/interface_add.c
+++ editor/src/interface_add.c
@@ -5,21 +5,33 @@
 ** Login   <merran_g@epitech.net>
 ** 
 ** Started on  Sun May 11 03:01:32 2014 Geoffrey Merran
-** Last update Sun May 11 16:50:23 2014 Geoffrey Merran
+** Last update Sun May 11 18:56:09 2014 Jeremy Mediavilla
 */
 
 #include "parser.h"
 
 void		on_clicked_add_champ(GtkWidget *addButton, t_winChampion *data)
 {
+  char		*error;
+
   (void) addButton;
-  add_champion(data, &game->champs);
+  error = check_champ_errors(data);
+  if (error == NULL)
+    add_champion(data, &game->champs);
+  else
+    printf("%s\n", error);
 }
 
 void		on_clicked_add_monster(GtkWidget *addButton, t_winMonster *data)
 {
+  char		*error;
+
   (void) addButton;
-  add_monster(data, &game->monsters);
+  error = check_monst_errors(data);
+  if (error == NULL)
+    add_monster(data, &game->monsters);
+  else
+    printf("%s\n", error);
 }
 
 void		on_clicked_add_room(GtkWidget *addButton, t_winRoom *data)
