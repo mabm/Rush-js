f102909678479b3a27acb9d627a3c5ec87d64326
charvo_a <charvo_a@epitech.eu>
2014-05-11 22:07:44 +0200

    charvo

--- visualizer/src/utils.c
+++ visualizer/src/utils.c
@@ -1,3 +1,13 @@
+/*
+** utils.c for  in /home/charvo_a/rush/visualizer/src
+**
+** Made by Nicolas Charvoz
+** Login   <charvo_a@epitech.net>
+**
+** Started on  Sun May 11 22:07:23 2014 Nicolas Charvoz
+** Last update Sun May 11 22:07:25 2014 Nicolas Charvoz
+*/
+
 #include "../hdr/sdl.h"
 
 int	center_text(char *str)
