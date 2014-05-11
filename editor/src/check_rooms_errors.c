c67b3b1f0335d16bc8b5b73f6dbe879f7ae90a9c
merran_g <merran_g@epitech.eu>
2014-05-11 23:35:02 +0200

    test

--- editor/src/check_rooms_errors.c
+++ editor/src/check_rooms_errors.c
@@ -5,7 +5,7 @@
 ** Login   <mediav_j@epitech.net>
 ** 
 ** Started on  Sun May 11 19:33:43 2014 Jeremy Mediavilla
-** Last update Sun May 11 21:21:26 2014 Jeremy Mediavilla
+** Last update Sun May 11 21:53:04 2014 Geoffrey Merran
 */
 
 #define _BSD_SOURCE
@@ -49,8 +49,8 @@ char		*check_room_error(t_winRoom *data)
     return ("Missing room name");
   else if (is_already_this_room((char *) text) == 1)
     return ("This room already exist");
-  text = gtk_combo_box_get_active_text(GTK_COMBO_BOX(data->combo_box));
-  if (text == NULL)
+  text = gtk_entry_get_text(GTK_ENTRY(data->attEntry[1]));
+  if (text == '\0')
     gtk_entry_set_text(GTK_ENTRY(data->attEntry[1]), "NONE");
   text = gtk_entry_get_text(GTK_ENTRY(data->attEntry[2]));
   if (text[0] == '\0')
